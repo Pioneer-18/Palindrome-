7a52d8426fb08fde86e9e17fced4e1ccc8eb7a57
Athira Nair <139950461+Pioneer-18@users.noreply.github.com>
2023-08-21 22:19:01 +0530

    Add files via upload
    Palindrome number is a number that remains the same when digits are reversed. For example, the number 12321 is a palindrome number
    Here, it checks whether the number is palindrome number or not.

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+x =int(input("Enter any number :"))
+y=int(str(x)[::-1])
+#loop                                                                 
+if(x==y):
+    print(" \n It is a Palindrome Number .")
+else:
+    print(" \n It is not a Palindrome Number. ")
+
+
+
+                                                            # AN
